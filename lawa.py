6fb4ff93b0e42aba1fb1eae5b74deb876eb5a13b
ShenDezhou <bangtech@sina.com>
2020-09-14 15:59:54 +0800

    1.lawa 四模式:全模式\精确\新词\搜索引擎.

--- lawa.py
+++ lawa.py
@@ -11,6 +11,13 @@ print("Full Mode: " + "/ ".join(seg_list))  # 全模式
 seg_list = lawa.cut("土地使用权转让，包括出售、赠与、互换；", cut_all=False)
 print("Default Mode: " + "/ ".join(seg_list))  # 精确模式
 
+seg_list = lawa.cut("中华人民共和国民法典", cut_all=False, HMM=True)
+print("HMM Mode(default on): " + "/ ".join(seg_list))  # 精确模式
+
+seg_list = lawa.cut("全国人民代表大会常务委员会关于授予在抗击新冠肺炎疫情斗争中作出杰出贡献的人士国家勋章和国家荣誉称号的决定 ", cut_all=False, HMM=True)
+print("HMM Mode(default on): " + "/ ".join(seg_list))  # 精确模式
+
+
 seg_list = lawa.cut("前款第二项土地使用权转让，不包括土地承包经营权和土地经营权的转移。")  # 默认是精确模式
 print(", ".join(seg_list))
 
